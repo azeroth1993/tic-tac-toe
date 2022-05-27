fefee038c1717737ed4bdc2dcb10020d50b02a00
Deltora72 <deltora72@outlook.com>
2022-05-27 16:57:09 +0200

    complete logic update + board size selection

--- src/components/game/board.js
+++ src/components/game/board.js
@@ -1,59 +1,96 @@
-import React, { useMemo, useState } from "react"
+import React, { useEffect, useState } from "react"
 import Cell from "./cell"
 import Button from '../button/button'
 
 const Board = ({ firstPlayer = 'x' }) => {
 
-  // Could provide a way for users to choose the first player to start the game (in future)
   const [nextPlayer, setNextPlayer] = useState(firstPlayer);
   const [currentPlayer, setCurrentPlayer] = useState('');
   const [winner, setWinner] = useState('');
-  const [winMove, setWinMove] = useState([]);
+  const [winMove, setWinMove] = useState({isWin: false, cells: []});
   const [scores, setScores] = useState({'x': 0, 'o': 0});
   const [gameOver, setGameOver] = useState(false);
-  const [clear, setClear] = useState(false);
+  const [clearCell, setClearCell] = useState(false);
   const [xMoves, setXMoves] = useState([]);
   const [oMoves, setOMoves] = useState([]);
-  const cells = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const winCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
-
-  const reset = () => {
-    setXMoves([]);
-    setOMoves([]);
-    setGameOver(false);
-    setClear(true);
-    setWinner(``);
+  const [rowSize, setRowSize] = useState(5);
+  const maxMoves = Math.ceil((rowSize ** 2) / 2);
+  const boardCells = [];
+  for (let i = 1; i < (rowSize ** 2) + 1; i++) {
+    boardCells.push(i);
   }
 
-  const winCheck = (player, moves) => {
-    let results = winCombinations.map(x => x.every(x => moves.includes(x)));
-    if (moves.length <= 5 && results.includes(true)) {
-      setWinMove(winCombinations[results.indexOf(true)]);
+  const checkWin = (moves, size) => {
+    moves.map(x => {
+      if ([x - 1, x, x + 1].every(y => moves.includes(y)) && (x % rowSize > 1)) {
+        setWinMove({isWin: true, cells: [x - 1, x, x + 1]});
+      }
+      if ([x - size, x, x + size].every(y => moves.includes(y)) && (x > rowSize) && ((rowSize ** 2 - x) > rowSize)) {
+        setWinMove({isWin: true, cells: [x - size, x, x + size]});
+      }
+      if ([x + (size - 1), x, x - (size - 1)].every(y => moves.includes(y)) && (x > rowSize) && (x % rowSize > 1) && ((rowSize ** 2 - x) > rowSize)) {
+        setWinMove({isWin: true, cells: [x + (size - 1), x, x - (size - 1)]});
+      }
+      if ([x + (size + 1), x, x - (size + 1)].every(y => moves.includes(y)) && (x > rowSize) && (x % rowSize > 1) && ((rowSize ** 2 - x) > rowSize)) {
+        setWinMove({isWin: true, cells: [x + (size + 1), x, x - (size + 1)]});
+      }
+    })
+  }
+
+  const checkMove = (moves, player) => {
+    if (moves.length > 0 && moves.length <= maxMoves && winMove.isWin) {
       setWinner(`player ${player} has won!`);
-      let newScore = player === 'x' ? {'x': scores.x + 1, 'o': scores.o} : {'x': scores.x, 'o': scores.o + 1};
+      let newScore = player === 'x' ? { 'x': scores.x + 1, 'o': scores.o } : { 'x': scores.x, 'o': scores.o + 1 };
       setScores(newScore);
       setGameOver(true);
-    } else if (moves.length > 0 && moves.length === 5 && !results.includes(true)) {
-      setWinMove([]);
+    } else if (moves.length > 0 && moves.length === maxMoves && !winMove.isWin) {
       setWinner(`it's a draw!`);
       setGameOver(true);
     }
   }
 
-  useMemo(() => {
-    winCheck(currentPlayer, xMoves);
-    winCheck(currentPlayer, oMoves);
-  }, [xMoves, oMoves]);
+  useEffect(() => {
+    checkMove(xMoves, currentPlayer);
+    checkMove(oMoves, currentPlayer);
+  }, [xMoves, oMoves, currentPlayer])
 
-  const handleMove = (index, content) => {
-    setClear(false);
-    content === 'x' ? setXMoves([...xMoves, index]) : setOMoves([...oMoves, index]);
+  const reset = () => {
+    setXMoves([]);
+    setOMoves([]);
+    setGameOver(false);
+    setClearCell(true);
+    setWinner(``);
+    setWinMove({isWin: false, cells: []});
+  }
+
+  const handleMove = (index, content, moves) => {
+    setClearCell(false);
+    let copyXMoves = xMoves;
+    let copyOMoves = oMoves;
+    content === 'x' ? copyXMoves.push(index) : copyOMoves.push(index);
+    content === 'x' ? setXMoves(copyXMoves) : setOMoves(copyOMoves);
     setCurrentPlayer(content);
     nextPlayer === 'x' ? setNextPlayer('o') : setNextPlayer('x');
+    checkWin(moves, rowSize);
+  }
+
+  const WelcomeScreen = () => {
+    return <div className="mb-3">
+      <label htmlFor="row-size" className="text-cream text-2xl font-bold mr-3 capitalize">board size :</label>
+      <select id="row-size" value={rowSize} onChange={(e) => setRowSize(e.target.value)} className="rounded-lg outline-none cursor-pointer py-1 px-2 text-lg bg-cell text-cream">
+        <option value={3}>3 x 3</option>
+        <option value={4}>4 x 4</option>
+        <option value={5}>5 x 5</option>
+        <option value={6}>6 x 6</option>
+        <option value={8}>8 x 8</option>
+        <option value={10}>10 x 10</option>
+      </select>
+    </div>
   }
 
   return (
     <div className="flex flex-col justify-center items-center w-full">
+      <WelcomeScreen />
       <h2 className="text-2xl font-bold text-center capitalize w-full mb-5 select-none text-cream">next player: <span className="text-rose-500">{nextPlayer}</span></h2>
       <div className="grid grid-cols-2 rounded-lg overflow-hidden mb-4 w-full">
         <div className="flex flex-col justify-center items-center">
@@ -65,9 +102,9 @@ const Board = ({ firstPlayer = 'x' }) => {
           <span className="block w-full text-center text-3xl font-bold text-cell bg-cream capitalize py-2">{scores.o}</span>
         </div>
       </div>
-      <div className={`grid grid-cols-3 gap-1 my-5 relative w-full rounded-lg ${gameOver ? 'pointer-events-none' : ''}`}>
-        {cells.map((x, i) => (
-          <Cell key={x + i} index={x} nextPlayer={nextPlayer} onClick={handleMove} reset={clear} isWin={gameOver && winMove.includes(x)} />
+      <div style={{ '--size': rowSize }} className={`grid [grid-template-columns:repeat(var(--size),_1fr)] gap-1 my-5 relative w-full rounded-lg ${gameOver ? 'pointer-events-none' : ''}`}>
+        {boardCells.map((x, i) => (
+          <Cell key={x + i} index={x} nextPlayer={nextPlayer} onClick={(index, content) => handleMove(index, content, (content === 'x' ? xMoves : oMoves))} reset={clearCell} isWin={gameOver && winMove.cells.includes(x)} />
         ))}
         <span 
           className={`${gameOver ? 'flex' : 'hidden'} justify-center items-center absolute top-0 left-0 w-full h-full z-10 bg-main-bg bg-opacity-90 text-cream capitalize text-3xl font-bold select-none`}
--- src/components/game/cell.js
+++ src/components/game/cell.js
@@ -17,13 +17,13 @@ const Cell = ({ index, nextPlayer, onClick, reset, isWin }) => {
 
   return (
     <span
-      className={`flex justify-center items-center w-full aspect-square cursor-pointer select-none active:scale-[0.97] transition-all rounded-lg ${isWin ? 'bg-white text-cell' : 'bg-cell text-cream'}`}
+      className={`flex justify-center items-center w-full aspect-square cursor-pointer select-none active:scale-[0.97] origin-center transition-transform duration-100 will-change-transform rounded-lg ${isWin ? 'bg-white text-cell' : 'bg-cell text-cream'}`}
       onClick={press}
     >
       {content === 'x' && 
-        <svg className="fill-current w-14 h-14 sm:w-20 sm:h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" /></svg>      }
+        <svg className="fill-current w-3/4 h-3/4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" /></svg>      }
       {content === 'o' &&
-        <svg className="fill-current w-14 h-14 sm:w-20 sm:h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 32.01c-123.5 0-224 100.5-224 224s100.5 224 224 224s224-100.5 224-224S347.5 32.01 224 32.01zM224 416c-88.22 0-160-71.78-160-160s71.78-159.1 160-159.1s160 71.78 160 159.1S312.2 416 224 416z" /></svg>      }
+        <svg className="fill-current w-3/4 h-3/4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 32.01c-123.5 0-224 100.5-224 224s100.5 224 224 224s224-100.5 224-224S347.5 32.01 224 32.01zM224 416c-88.22 0-160-71.78-160-160s71.78-159.1 160-159.1s160 71.78 160 159.1S312.2 416 224 416z" /></svg>      }
     </span>
   )
 
